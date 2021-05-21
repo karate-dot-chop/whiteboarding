a94643ff819db7e351a0cac82109cc72b0f36fed
dani <dani.zaghian@gmail.com>
2021-05-21 14:08:11 -0700

    adds more decomp

--- w04.rb
+++ w04.rb
@@ -9,3 +9,16 @@
 # Write a function that takes in 2 strings and print each string
 # Write a function that takes in 2 strings and loops through each and prints out each character
 # Write a function that takes in 2 strings and loops through the first string, then loops through the second string and compares each letter and prints "match!" if they match
+
+
+# Write a function that takes in an array of numbers and returns a new array of the same numbers without any duplicates.
+
+# clarify
+# Input: array of nums => [1, 1, 3, 5, 1, 2, 6, 1, 6]
+# Ouput: array of nums => [1, 3, 5, 2, 6]
+
+# decomposition
+# Write a function that takes in an array of numbers and prints it out
+# Write a function that takes in an array of numbers and loops through it and prints out each number
+# Write a function that takes in an array of numbers and loops through it and compares each number to the first number in the array, and prints match if there's a match
+# Write a function that takes in an array of numbers and loops through each number, with a second inner loop that compares the current_number to each other number and prints match if they match
