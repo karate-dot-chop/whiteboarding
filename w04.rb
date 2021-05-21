6f043cc90ec4cca2ffc921e19b77d5e641dfaefd
dani <dani.zaghian@gmail.com>
2021-05-21 13:41:10 -0700

    adds decomposition

---
+++ w04.rb
@@ -0,0 +1,11 @@
+# Write a function that takes in two strings and returns the longest common substring.
+
+# clarify
+# Input: 2 strings => "trend", "brenda"
+# Output: string => "rend"
+
+# visualize
+# Write a function that prints "hello"
+# Write a function that takes in 2 strings and print each string
+# Write a function that takes in 2 strings and loops through each and prints out each character
+# Write a function that takes in 2 strings and loops through the first string, then loops through the second string and compares each letter and prints "match!" if they match
