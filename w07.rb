02559d42becda70ca9ccc2ff2c00d7976e5b1f29
dani <dani.zaghian@gmail.com>
2021-06-10 12:31:51 -0700

    adds w07 decomp"

---
+++ w07.rb
@@ -0,0 +1,14 @@
+# Write a function that takes in a string of code with parentheses, square brackets, and curly brackets, and checks if the code is balanced with correct bracket syntax.
+
+# clarify
+# example input:
+# "(array = ][, object = {}, string = ''"
+# "(x + [1, { a: 2, b: 3}])"
+# expected output:
+# false
+# true
+
+# visualize
+# Write a function that takes in a string of code and counts how many open parenthesis are in the string
+# Write a function that takes in a string of code and counts how many open parenthesis, curly bracets, and square brackets are in the string, as well as how many close parenthesis, curly bracets, and square brackets
+
