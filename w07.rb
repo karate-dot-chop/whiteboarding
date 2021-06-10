28e47e5b1d43a9710774588517a7152ddee9a606
dani <dani.zaghian@gmail.com>
2021-06-10 13:37:07 -0700

    adds decomp problem 2

--- w07.rb
+++ w07.rb
@@ -12,3 +12,17 @@
 # Write a function that takes in a string of code and counts how many open parenthesis are in the string
 # Write a function that takes in a string of code and counts how many open parenthesis, curly bracets, and square brackets are in the string, as well as how many close parenthesis, curly bracets, and square brackets
 
+
+# Write a function that takes in two arrays of numbers and returns all the numbers that appear in both arrays.
+
+# clarify
+# example input: array1 = [-12, 45, 2, 0] array2 = [2, 6, 22, 10, 45]
+# expected output: [2, 45]
+
+# visualize
+# Write a function that takes in two arrays of numbers and prints out each array
+# Write a function that takes in two arrays of numbers and prints out each number in each array one at a time
+# Write a function that takes in two arrays of numbers and compares the first number in the first array to each number in the second array, and prints "match" if they are the same
+# Write a function that takes in two arrays of numbers, loops through the first array and compares each number to each number in the second array and prints "match" if they match (n^2)
+# Other version
+# Write a function that takes in 2 arrays of numbers and loops through the first array and keeps track of the times each number appears in a hash (2n)
